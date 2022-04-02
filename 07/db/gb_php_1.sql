16bd25fdad67ab3a7a827875c6aed7a55b4106fa
alexmacbook <alex_yasinsky@mail.ru>
2022-04-02 16:08:41 +0300

    02/04/2022 task_7 done

--- 07/db/gb_php_1.sql
+++ 07/db/gb_php_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:8889
--- Время создания: Мар 30 2022 г., 20:12
+-- Время создания: Апр 02 2022 г., 13:07
 -- Версия сервера: 5.7.34
 -- Версия PHP: 7.4.21
 
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- База данных: `gb_php_1`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `basket`
+--
+
+CREATE TABLE `basket` (
+  `id` int(11) NOT NULL,
+  `session_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `basket`
+--
+
+INSERT INTO `basket` (`id`, `session_id`, `product_id`, `amount`) VALUES
+(19, 3, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -41,9 +61,9 @@ CREATE TABLE `catalog` (
 --
 
 INSERT INTO `catalog` (`id`, `name`, `price`, `image`, `about`, `views`) VALUES
-(1, 'Яблоко', '5', 'apple.jpg', 'Сочное яблоко', 52),
-(2, 'Пицца', '10', 'pizza.jpeg', 'Пицца как пицца, ничего особенного', 101),
-(5, 'Чай', '3', 'tea.png', ' Чай! Индийский чай! Грузинский чай! ', 8);
+(1, 'Яблоко', '5', 'apple.jpg', 'Сочное яблоко', 72),
+(2, 'Пицца', '10', 'pizza.jpeg', 'Пицца как пицца, ничего особенного', 284),
+(5, 'Чай', '3', 'tea.png', ' Чай! Индийский чай! Грузинский чай! ', 14);
 
 -- --------------------------------------------------------
 
@@ -67,32 +87,43 @@ INSERT INTO `feedback` (`id`, `product_id`, `name`, `text`, `time`) VALUES
 (1, 1, 'Валера404', 'Кислющие!!!', '2022-03-30 08:11:41'),
 (3, 5, 'Сэр Ричард', 'Ай ай ай ай!', '2022-03-30 08:15:11'),
 (4, 1, 'Милашка', 'ну еклм, какая няма', '2022-03-30 17:37:43'),
-(19, 2, 'Валера404', 'йоу, братва! пицца - пушка!', '2022-03-30 23:07:52');
+(19, 2, 'Валера404', 'йоу, братва! пицца - пушка!', '2022-03-30 23:07:52'),
+(20, 2, 'Бедолага', 'Мало колбасы', '2022-03-31 10:33:04'),
+(21, 2, 'Мамонт', 'ну балдеж, как в той кафешке во Флоренции', '2022-03-31 16:47:10'),
+(24, 2, 'Эклер', 'Пицца сосет!!!', '2022-03-31 17:12:12'),
+(27, 2, 'Мамонт', 'А я вот эклеры не люблю', '2022-03-31 17:26:07');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `gallery`
+-- Структура таблицы `users`
 --
 
-CREATE TABLE `gallery` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `views` int(11) NOT NULL DEFAULT '0'
+  `login` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `role` varchar(127) NOT NULL,
+  `cookie_hash` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `gallery`
+-- Дамп данных таблицы `users`
 --
 
-INSERT INTO `gallery` (`id`, `name`, `views`) VALUES
-(1, 'apple.jpg', 4),
-(2, 'pizza.jpeg', 10),
-(12, 'tea.png', 5);
+INSERT INTO `users` (`id`, `login`, `password_hash`, `role`, `cookie_hash`) VALUES
+(3, 'admin', '$argon2id$v=19$m=65536,t=4,p=1$MzJWM245WTBoMTBLR2xWYQ$TLTk7pGR1N+eLNWlyUzbgHZOoUV7KFnseZV1CJoF7iQ', 'admin', '103039392262460f32f3e102.15492155'),
+(4, 'user1', '$argon2id$v=19$m=65536,t=4,p=1$LnJhWmZhTXlwT05xSjFpNg$a9DT9iXqxvAWO9WctHuz4pN165zyEI+Mlih6PgK5RjA', 'user', NULL);
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `basket`
+--
+ALTER TABLE `basket`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `catalog`
@@ -108,15 +139,20 @@ ALTER TABLE `feedback`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `gallery`
+-- Индексы таблицы `users`
 --
-ALTER TABLE `gallery`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `basket`
+--
+ALTER TABLE `basket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT для таблицы `catalog`
@@ -128,13 +164,13 @@ ALTER TABLE `catalog`
 -- AUTO_INCREMENT для таблицы `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
--- AUTO_INCREMENT для таблицы `gallery`
+-- AUTO_INCREMENT для таблицы `users`
 --
-ALTER TABLE `gallery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
